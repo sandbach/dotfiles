5aea8499513f54e718201c87f494f7cdeb6fabeb
Asher Sandbach <ajsandbach@gmail.com>
2024-03-04 17:38:21 +0800

    Removed workspaces from Doom
    Workspaces were getting in the way of opening files with emacsclient.

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -49,15 +49,15 @@
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
-       workspaces        ; tab emulation, persistence & separate workspaces
+       ;; workspaces        ; tab emulation, persistence & separate workspaces
        ;;zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
        ;; file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       format
-       ;; (format +onsave)  ; automated prettiness
+       ;; format
+       (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
